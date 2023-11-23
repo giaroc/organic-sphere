25d9df61351f84a687bbc5aae5b142ec0d807154
giacomo.rocco <gr@nethive.it>
2023-11-23 12:49:46 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Three Organic Sphere
+
+This project present an Organic Sphere made with Three.js. 
+This work is built upon [shaders-crash-course ](https://github.com/visionary-3d/shaders-crash-course) by [Visionary 3D ](https://github.com/visionary-3d), who created and contributed to the original project.
+
+
+## Installation
+
+Clone this repo and npm install.
+
+```bash
+npm i
+```
+
+## Usage
+
+### Development server
+
+```bash
+npm start
+```
+
+You can view the development server at `localhost:8080`.
+
+### Production build
+
+```bash
+npm run build
+```
+
+> Note: Install [http-server](https://www.npmjs.com/package/http-server) globally to deploy a simple server.
+
+```bash
+npm i -g http-server
+```
+
+You can view the deploy by creating a server in `dist`.
+
+```bash
+cd dist && http-server
+```
+
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
---
+++ src/app.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three'
+import {addPass, useCamera, useGui, useRenderSize, useScene, useTick} from './render/init.js'
+// import postprocessing passes
+import {SavePass} from 'three/examples/jsm/postprocessing/SavePass.js'
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass.js'
+import {BlendShader} from 'three/examples/jsm/shaders/BlendShader.js'
+import {CopyShader} from 'three/examples/jsm/shaders/CopyShader.js'
+import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
+import vertexPars from './shaders/vertex_pars.glsl'
+import vertexMain from './shaders/vertex_main.glsl'
+import fragmentPars from './shaders/fragment_pars.glsl'
+import fragmentMain from './shaders/fragment_main.glsl'
+
+const startApp = () => {
+    const scene = useScene()
+    // const camera = useCamera()
+    // const gui = useGui()
+    const {width, height} = useRenderSize()
+
+    // settings
+    const MOTION_BLUR_AMOUNT = 0.725
+
+    // lighting
+    const dirLight = new THREE.DirectionalLight('#01B08E', 0.6)
+    dirLight.position.set(2, 2, 2)
+
+    const ambientLight = new THREE.AmbientLight('#01B08E', 0.27)
+    scene.add(dirLight, ambientLight)
+
+
+    // meshes
+    const geometry = new THREE.IcosahedronGeometry(1, 400)
+    const material = new THREE.MeshStandardMaterial({
+        onBeforeCompile: (shader) => {
+            // storing a reference to the shader object
+            material.userData.shader = shader
+
+            // uniforms
+            shader.uniforms.uTime = {value: 0}
+
+            const parsVertexString = /* glsl */ `#include <displacementmap_pars_vertex>`
+            shader.vertexShader = shader.vertexShader.replace(
+                parsVertexString,
+                parsVertexString + vertexPars
+            )
+
+            const mainVertexString = /* glsl */ `#include <displacementmap_vertex>`
+            shader.vertexShader = shader.vertexShader.replace(
+                mainVertexString,
+                mainVertexString + vertexMain
+            )
+
+            const mainFragmentString = /* glsl */ `#include <normal_fragment_maps>`
+            const parsFragmentString = /* glsl */ `#include <bumpmap_pars_fragment>`
+            shader.fragmentShader = shader.fragmentShader.replace(
+                parsFragmentString,
+                parsFragmentString + fragmentPars
+            )
+            shader.fragmentShader = shader.fragmentShader.replace(
+                mainFragmentString,
+                mainFragmentString + fragmentMain
+            )
+        },
+    })
+
+    const ico = new THREE.Mesh(geometry, material)
+    scene.add(ico)
+
+    ico.interactive = true;
+    const followMouseMovement = (event) => {
+        const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+        const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        const movementX = mouseX * 0.2;
+        const movementY = mouseY * 0.2;
+
+        // Applica il movimento alla sfera
+        ico.position.x = movementX;
+        ico.position.y = movementY;
+        ico.rotation.x = movementY * 2;
+        ico.rotation.y = movementX * 2;
+    };
+
+    window.addEventListener('mousemove', followMouseMovement);
+
+    // GUI
+    //const cameraFolder = gui.addFolder('Camera')
+    // cameraFolder.add(camera.position, 'z', 0, 10)
+    //cameraFolder.open()
+
+    // postprocessing
+    const renderTargetParameters = {
+        minFilter: THREE.LinearFilter,
+        magFilter: THREE.LinearFilter,
+        stencilBuffer: false,
+    }
+
+
+    // postprocessing
+    addPass(new UnrealBloomPass(new THREE.Vector2(width, height), 0.7, 0.4, 0.4))
+
+    useTick(({timestamp, timeDiff}) => {
+        const time = timestamp / 5000
+        material.userData.shader.uniforms.uTime.value = time
+
+    })
+}
+
+
+export default startApp
+
